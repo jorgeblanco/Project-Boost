1229df85929503693d1b26d73d9646a6aef84472
Jorge Blanco <jbsfreelance@gmail.com>
2019-09-11 23:01:15 -0700

    Initial commit
    Added a basic ship and some thrust

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float thrustForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        
+        // Thrust control
+        if (Input.GetButton("Jump"))
+        {
+            rb.AddRelativeForce(Vector3.up * thrustForce);
+        }
+        
+        // Rotation control
+        if (horizontal > 0)
+        {
+        }
+        else if (horizontal < 0)
+        {
+        }
+    }
+}
