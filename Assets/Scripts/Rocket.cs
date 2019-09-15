f7fdec361f8a4c4cda9ec74dc3c7072acdb95c2a
Jorge Blanco <jbsfreelance@gmail.com>
2019-09-14 21:54:54 -0700

    Added better rocket controls

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,40 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    private Rigidbody rb;
+    private Rigidbody _rb;
+    private AudioSource _audioSource;
     [SerializeField] private float thrustForce;
+    [SerializeField] private float rotationForce;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        ProcessRotation();
+        ProcessThrust();
     }
 
-    private void ProcessInput()
+    private void ProcessRotation()
     {
         var horizontal = Input.GetAxis("Horizontal");
-        
-        // Thrust control
-        if (Input.GetButton("Jump"))
-        {
-            rb.AddRelativeForce(Vector3.up * thrustForce);
-        }
+
+        // Take manual control of rotation
+        _rb.freezeRotation = true;
         
         // Rotation control
         if (horizontal > 0)
         {
+            transform.Rotate(Time.deltaTime * rotationForce * -Vector3.forward);
         }
         else if (horizontal < 0)
         {
+            transform.Rotate(Time.deltaTime * rotationForce * Vector3.forward);
+        }
+        
+        // Resume physics control of rotation
+        _rb.freezeRotation = false;
+    }
+
+    private void ProcessThrust()
+    {
+// Thrust control
+        if (Input.GetButton("Jump"))
+        {
+            _rb.AddRelativeForce(Time.deltaTime * thrustForce * Vector3.up);
+            if (!_audioSource.isPlaying)
+            {
+                _audioSource.Play();
+            }
+        }
+        else if (_audioSource.isPlaying)
+        {
+            _audioSource.Stop();
         }
     }
 }
