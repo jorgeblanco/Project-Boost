8a5bb6ff798160bc24ad71d2ed63ea4b4b6db5cd
Jorge Blanco <jbsfreelance@gmail.com>
2019-09-14 23:40:19 -0700

    Added a win condition and better transitions

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,28 +1,43 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     private Rigidbody _rb;
     private AudioSource _audioSource;
+    private int _sceneCount;
     [SerializeField] private float thrustForce = 2000f;
     [SerializeField] private float rotationForce = 200f;
+    [SerializeField] private float sceneLoadTime = 1f;
+    
+    private enum State
+    {
+       Alive,
+       Dying,
+       Transcending
+    }
 
-    // Start is called before the first frame update
+    private State _state = State.Alive;
+
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
         _audioSource = GetComponent<AudioSource>();
+        InitScenes();
+    }
+    
+    private void InitScenes()
+    {
+        _sceneCount = SceneManager.sceneCountInBuildSettings;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        ProcessRotation();
-        ProcessThrust();
+        if (_state == State.Alive)
+        {
+            ProcessRotation();
+            ProcessThrust();
+        }
     }
 
     private void ProcessRotation()
@@ -65,14 +80,34 @@ public class Rocket : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        if(_state != State.Alive){return;}
+        
         if (collision.gameObject.GetComponent<Friendly>() == null)
         {
             // Reload level upon death
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            _state = State.Dying;
+            _audioSource.Stop();  // Kill the sound if dying
+            Invoke(nameof(ReloadScene), sceneLoadTime);
+        }
+        else if (collision.gameObject.GetComponent<Finish>() != null)
+        {
+            // Reload level upon death
+            _state = State.Transcending;
+            Invoke(nameof(LoadNextScene), sceneLoadTime);
         }
         else
         {
             Debug.Log("Friendly :)");
         }
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % _sceneCount);
     }
 }
